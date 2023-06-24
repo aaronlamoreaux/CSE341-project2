d19fd6ab5db5ac240e428f282c88ee151a66546a
aaronlamoreaux <lam22013@byui.edu>
2023-06-24 13:48:41 -0700

    assignment 5

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Quote Book API',
         description: 'CSE 341 project 2 API'
     },
-    host: 'cse341-project2-aaronlamoreaux.onrender.com/',
+    host: 'cse341-project2-aaronlamoreaux.onrender.com',
     schemes: ['https']
 };
 
